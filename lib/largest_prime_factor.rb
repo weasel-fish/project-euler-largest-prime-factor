6ad1e53ab8cb1fcaaa80fb43cd9734e1bb951536
github username <github email address>
2021-09-29 11:57:08 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,12 @@
 # Enter your procedural solution here!
+
+def largest_prime_factor(num)
+    
+    current = num
+
+    (2..Math.sqrt(num).to_i).each do |i|
+        current /= i if current % i == 0 && current / i > 1
+    end
+
+    current
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    attr_accessor :num
+
+    def initialize(num)
+        @num = num
+    end
+
+    def number
+        current = num
+
+        (2..Math.sqrt(num).to_i).each do |i|
+            current /= i if current % i == 0 && current / i > 1
+        end
+    
+        current
+    end
+
+end
